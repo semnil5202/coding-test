ca3660abe7cd892b9bedf8184bb7c1d5b91dd413
Replit user <>
2022-09-06 07:30:12 +0000

    init

---
+++ init/readline.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function(input) {
+  console.log(+input)
+
+  rl.close();
+}).on("close", function() {
+  process.exit();
+});
