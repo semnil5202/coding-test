912eccddd92571cd87de2579c8c67492023c36dc
semnil5202 <semnil5202@gmail.com>
2022-09-09 01:18:22 +0900

    Greedy Init

--- Greedy/11047_동전0.js
+++ Greedy/11047_동전0.js
@@ -1,17 +1,23 @@
 // https://www.acmicpc.net/problem/11047
 
-let coinType = require("fs").readFileSync("/dev/stdin").toString().trim().split(/\s/);
+let coinType = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(/\s/);
 
 let coinSum = +coinType[1];
 let count = 0;
 
 for (let i = coinType.length - 1; i > 1; i--) {
-  if ( coinSum % +coinType[i] !== coinSum ) {
+  if (coinSum % +coinType[i] !== coinSum) {
     let max = +coinType[i];
     count = count + parseInt(coinSum / max);
     coinSum = parseInt(coinSum % max);
-    
-    if ( coinSum === 0 ) { break; }
+
+    if (coinSum === 0) {
+      break;
+    }
   }
 }
 
--- Greedy/2839_설탕배달.js
+++ Greedy/2839_설탕배달.js
@@ -1,16 +1,15 @@
 // https://www.acmicpc.net/problem/2839
 
-let input = require('fs').readFileSync('/dev/stdin').toString();
+let input = require("fs").readFileSync("/dev/stdin").toString();
 
 let weight = +input;
 let count = 0;
 
 while (true) {
   if (weight % 5 === 0) {
-    count += (weight / 5);
+    count += weight / 5;
     break;
-  }
-  else {
+  } else {
     if (weight < 0) {
       count = -1;
       break;
