efd3cdea04edd6102608828b4cb31740c95eabc3
semnil5202 <semnil5202@gmail.com>
2022-10-21 22:28:36 +0900

    2309

--- BAEKJOON/Bruteforcing/2309_일곱난쟁이.js
+++ BAEKJOON/Bruteforcing/2309_일곱난쟁이.js
@@ -1,19 +1,33 @@
 // https://www.acmicpc.net/problem/2309
 
-let a = require("fs").readFileSync("./INPUT.txt").toString().trim().split("\n");
+let [...input] = require("fs")
+  .readFileSync("./INPUT.txt")
+  .toString()
+  .trim()
+  .split("\n");
 
-const Total = a.reduce((r, v) => {
-  return r + v;
-}, 0);
-let answer = "";
-for (let i = 0; i < 9; i++) {
-  if (answer.length > 0) break;
-  for (let j = i + 1; j < 9; j++) {
-    if (Total - a[i] - a[j] == 100) {
-      const truth = a.filter((_, k) => k != i && k != j).sort((a, b) => a - b);
-      answer = truth.join("\n");
-      console.log(answer);
+const arr = input.map((i) => Number(i));
+
+const sum = arr.reduce((pre, cur) => pre + cur, 0);
+let falseDwarf1 = 0;
+let falseDwarf2 = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 1; j < arr.length; j++) {
+    let tempSum = sum;
+
+    if (tempSum - arr[i] - arr[j] === 100) {
+      falseDwarf1 = arr[i];
+      falseDwarf2 = arr[j];
+
+      arr.splice(i, 1);
+      arr.splice(j - 1, 1);
       break;
     }
   }
+  if (falseDwarf1 > 0) break;
 }
+
+arr.sort((a, b) => a - b);
+
+console.log(arr.join("\n"));
