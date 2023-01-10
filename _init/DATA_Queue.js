7eaec3530918bc3fbdfb7c6741aba6ea3aed9020
semnil5202 <semnil5202@gmail.com>
2023-01-10 23:42:47 +0900

    feat: 자바스크립트 큐 구현

---
+++ _init/DATA_Queue.js
@@ -0,0 +1,42 @@
+class Node {
+  value;
+  prev = null;
+  next = null;
+
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Queue {
+  size = 0;
+  start = null;
+  end = null;
+
+  enQueue(value) {
+    const newNode = new Node(value);
+
+    if (this.start === null) this.start = newNode;
+    else {
+      this.end.next = newNode;
+      newNode.prev = this.end;
+    }
+
+    this.end = newNode;
+    this.size += 1;
+
+    return newNode;
+  }
+
+  deQueue() {
+    const value = this.start.value;
+
+    if (this.start === null) return null;
+    if (this.start === this.end) this.end = null;
+
+    this.start = this.start.next;
+    this.size -= 1;
+
+    return value.value;
+  }
+}
