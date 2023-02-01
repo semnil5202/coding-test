41f9c727ba8a2d0e65efcc5ff3f6d7a333aeacce
semnil5202 <semnil5202@gmail.com>
2023-02-01 23:54:17 +0900

    refactor(1931): 오답 문제 재풀이

--- BAEKJOON/Greedy/1931_회의실배정.js
+++ BAEKJOON/Greedy/1931_회의실배정.js
@@ -1,9 +1,22 @@
 // https://www.acmicpc.net/problem/1931
 
-const [n, ...arr] = require("fs")
-  .readFileSync("./INPUT.txt")
-  .toString()
-  .trim()
-  .split("\n");
+const [n, ...arr] = require('fs').readFileSync('./INPUT.txt').toString().trim().split('\n');
+const meetings = arr.map((el) => el.split(' ').map((el) => Number(el)));
+let count = 1;
 
-console.log(arr);
+meetings.sort((a, b) => {
+  if (a[1] - b[1] === 0) return a[0] - b[0];
+  return a[1] - b[1];
+});
+
+let meetNow = meetings[0];
+
+for (let i = 1; i < meetings.length; i++) {
+  const end = meetings[i];
+  if (meetNow[1] <= end[0]) {
+    meetNow = meetings[i];
+    count++;
+  }
+}
+
+console.log(count);
